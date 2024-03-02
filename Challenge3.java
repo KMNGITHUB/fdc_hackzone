49c4c947a457d3a3e5dcff27b299a2e9b2aef9d0
Krishnan Mekkat Neelakandan <kadampuzhas@gmail.com>
2024-03-02 12:02:37 +0530

    Add files via upload

---
+++ Challenge3.java
@@ -0,0 +1,29 @@
+package com.hackazone.challenge1;
+
+import com.financial.cleaningrobot.api.RobotApi;
+import com.financial.cleaningrobot.api.RobotControl;
+
+/**
+ * Robot controller implementation
+ */
+public class Challenge3 {
+
+    /**
+     * The main entry point.
+     * Don't change the code, besides the file path of the room layout.
+     */
+    public static void main(String[] args) throws Exception {
+        RobotControl rc = new RobotControl("../resources/room-layout-1.txt");
+        RobotApi api = rc.getRobotApi();
+        cleanRooms(api);
+        rc.evaluateResult();
+    }
+
+    /**
+     * Add your implementation here.
+     */
+    private static void cleanRooms(RobotApi api) {
+       
+    }
+}
+
